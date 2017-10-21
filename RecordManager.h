7d4a7c449f5c254a313e41801ade141b5426e17c
jzc15 <jzc15@mails.tsinghua.edu.cn>
2017-10-21 14:26:01 +0800

    底层数据表维护文件完成

--- RecordManager.h
+++ RecordManager.h
@@ -17,11 +17,16 @@
 using namespace std;
 /*
  *  接口总结
- *  RecordManger(string databaseName)   构造一个数据库，创建数据库文件夹
+ *  RecordManger(string databaseName)   
+ *      构造一个数据库，创建数据库文件夹
  *  createRecord(string tableName, ushortList buf, BufLength length)  
  *      添加数据，如果数据表不存在则创建数据表，返回值表示是否添加成功
  *  searchRecord(string tableName, ushort PageNo, ushort SlotNo, ushort &length)
  *      根据页编号和槽编号查询数据，返回ushortList指针，以及数据长度
+ *  deleteRecord(string tableName, ushort PageNo, ushort SlotNo)
+ *      根据页编号和槽编号查询数据，删除数据
+ *  nextRecord(string tableName, ushort& PageNo, ushort& SlotNo)
+ *      根据页编号和槽编号查询下一条数据，返回是否存在下一条
  */
 class RecordManager
 {
@@ -31,6 +36,7 @@ public:
     {
         this->databaseName = "debug";
     }
+
     RecordManager(string databaseName)
     {
         this->databaseName = databaseName;
@@ -38,6 +44,7 @@ public:
         mkdir(dirname.c_str() ,0755);
         
     }
+
     bool createRecord(string tableName, ushortList buf, BufLength length)
     {
         FileManager f;
@@ -120,7 +127,7 @@ public:
          * 获取第0页，检查PageNo是否合法
          */
         BufType b0 = bpm.getPage(fileID, 0, index);
-        bpm.markDirty(index);
+        bpm.access(index);
         ushortList t = (ushortList) b0;
         
         if(b0[0] < PageNo || PageNo <= 0)
@@ -130,7 +137,7 @@ public:
          * 获取第i页，判断SlotNo是否合法，查询数据
          */
         BufType bi = bpm.getPage(fileID, PageNo, index);
-        bpm.markDirty(index);
+        bpm.access(index);
         ushortList ti = (ushortList) bi;
         
         if(ti[0] < SlotNo || PageNo <= 0)
@@ -194,6 +201,58 @@ public:
         bpm.close();
         f.closeFile(fileID);
         return true;
+    }
+
+    bool nextRecord(string tableName, ushort& PageNo, ushort& SlotNo)
+    {
+        FileManager f;
+        string filepath = databaseName + "/" + tableName;
+        int fileID;
+        f.createFile(filepath.c_str());
+        f.openFile(filepath.c_str(), fileID);
+        BufPageManager bpm(&f);
+        int index;
+        /*
+         * 获取第0页，检查PageNo是否合法
+         */
+        BufType b0 = bpm.getPage(fileID, 0, index);
+        bpm.access(index);
+        ushortList t = (ushortList) b0;
+        
+        if(b0[0] < PageNo || PageNo <= 0)
+            return NULL;
+        
+        /*
+         * 获取第i页，判断SlotNo是否合法，查询数据
+         */
+        BufType bi = bpm.getPage(fileID, PageNo, index);
+        bpm.access(index);
+        ushortList ti = (ushortList) bi;
+        
+        if(ti[0] < SlotNo || PageNo <= 0)
+            return NULL;
+        
+        while(true)
+        {
+            if(SlotNo < ti[0])
+            {
+                SlotNo ++;
+                return true;
+            }
+            else if (PageNo < t[0])
+            {
+                PageNo ++;
+                BufType bi = bpm.getPage(fileID, PageNo, index);
+                bpm.access(index);
+                ushortList ti = (ushortList) bi;
+                SlotNo = 0;
+            }
+            else
+                return false;
+        }
+        bpm.close();
+        f.closeFile(fileID);
+        return false;
     }
 };
 
